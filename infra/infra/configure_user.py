a84296455f2632a75223348db3819da47fac36fb
Jonathan E. Magen <yonkeltron@gmail.com>
2021-08-08 15:32:58 -0400

    improvement to user config
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

--- infra/infra/configure_user.py
+++ infra/infra/configure_user.py
@@ -1,16 +1,31 @@
+from pyinfra import host
+from pyinfra.facts import files
 from pyinfra.operations import git, server
 
-server.shell(
-    name='Install and set up Volta',
-    commands=[
-        'curl https://get.volta.sh | bash',
-        'volta setup'
-    ]
+if host.get_fact(files.Directory, '/home/yonkeltron/.volta'):
+    server.shell(
+        name='Install and set up Volta',
+        commands=[
+            'curl https://get.volta.sh | bash',
+            'volta setup'
+        ]
+    )
+
+git.config(
+    name='Set git name',
+    key='user.name',
+    value='Jonathan E. Magen'
+)
+
+git.config(
+    name='Set git email',
+    key='user.email',
+    value='yonkeltron@gmail.com'
 )
 
 git.repo(
     name='Clone hvs-ship-console',
-    src='',
-    dest='',
+    src='https://github.com/yonkeltron/hvs-ship-console.git',
+    dest='/opt/aith/hvs-ship-console',
     branch='main'
 )
