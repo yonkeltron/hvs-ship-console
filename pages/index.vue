7412e5383fe7fbd65ae61d488eb5c2ed7d6e9d09
Jonathan E. Magen <yonkeltron@gmail.com>
2021-08-09 20:50:27 -0400

    images and landing
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <Tutorial/>
+  <section class="p-5 font-mono text-white bg-black h-full">
+    <h2 class="text-4xl">&rarr; All Aboard!</h2>
+    <div class="grid justify-items-center">
+      <img src="images/new_wave.svg" />
+    </div>
+  </section>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
