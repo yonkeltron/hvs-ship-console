480a7fe61db5a6e3339905d6ba51683d002857f0
Jonathan E. Magen <yonkeltron@gmail.com>
2021-08-10 21:28:59 -0400

    dice
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

---
+++ components/die-display.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex-1 border-4 border-white rounded">
+    <div class="flex h-48 justify-center items-center">
+      <p class="text-9xl text-white font-bold">{{ dieFace }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  props: {
+    face: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      dieFace: props.face,
+    };
+  },
+});
+</script>
---
+++ pages/dice.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="p-5 font-mono text-white bg-black h-screen">
+    <div class="flex gap-5">
+      <DieDisplay :face="roll()" />
+      <DieDisplay :face="roll()" />
+      <DieDisplay :face="roll()" />
+      <DieDisplay :face="roll()" />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    const roll = () => {
+      const rand = Math.floor(Math.random() * 3);
+
+      let output = '';
+      if (rand === 0) {
+        output = '-';
+      } else if (rand === 1) {
+        output = '+';
+      }
+
+      return output;
+    };
+
+    return { roll };
+  },
+});
+</script>
